6e573fbc92f5832c270bd996ac776d15b41e6d94
Salma Sedrati <salmasedrati18@gmail.com>
2025-12-28 00:30:06 +0100

    Initialisation Eureka Server et MS Cours

---
+++ ms-cours/src/main/java/com/example/mscours/MsCoursApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCoursApplication.class, args);
+    }
+
+}
---
+++ ms-cours/src/test/java/com/example/mscours/MsCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
