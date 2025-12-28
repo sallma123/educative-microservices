5158f5071a9f4144ba54011286d82e3ffeb08301
Naadsss <nada.garouat@emsi-edu.ma>
2025-12-28 22:34:21 +0100

    add EtudiantRepository

---
+++ ms-etudiants/src/main/java/com/educative/etudiants/repository/EtudiantRepository.java
@@ -0,0 +1,9 @@
+package com.educative.etudiants.repository;
+
+import com.educative.etudiants.entity.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+}
