a158c2b63a0fc0499767fd449dbd7d107f9da124
Naadsss <nada.garouat@emsi-edu.ma>
2025-12-28 18:35:39 +0100

    Initialisation ms-etudiants : démarrage ok et config min

---
+++ ms-etudiants/ms-etudiants/src/main/java/com/educative/ms_etudiants/MsEtudiantsApplication.java
@@ -0,0 +1,13 @@
+package com.educative.ms_etudiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEtudiantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEtudiantsApplication.class, args);
+	}
+
+}
---
+++ ms-etudiants/ms-etudiants/src/test/java/com/educative/ms_etudiants/MsEtudiantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educative.ms_etudiants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEtudiantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
